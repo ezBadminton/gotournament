0c42adca6412468194d82a7f7559266b17b5b6d1
Simon Gerstmeier <simon@gerstmeier.dev>
2025-01-24 12:18:58 +0100

    Implemented badminton score

---
+++ badminton/score.go
@@ -0,0 +1,154 @@
+package badminton
+
+import (
+	"errors"
+
+	"github.com/ezBadminton/gotournament/internal"
+)
+
+var (
+	ErrPointsZero = errors.New("winning points are zero or less")
+	ErrSetsZero   = errors.New("winning sets are zero or less")
+	ErrMaxPoints  = errors.New("max points are less than winning points")
+
+	ErrUndetermined = errors.New("the winner is undeterminable from the score")
+
+	ErrEmpty           = errors.New("empty score")
+	ErrUndeterminedSet = errors.New("a set has equal points")
+	ErrUnequalSets     = errors.New("opponents have unequal number of sets")
+	ErrTooManySets     = errors.New("too many sets")
+	ErrTooFewSets      = errors.New("too few sets")
+	ErrNegativePoints  = errors.New("negative points")
+	ErrTooManyPoints   = errors.New("points exceed the max points setting")
+	ErrTooFewPoints    = errors.New("set winner points are less than the winning point setting")
+	ErrInvalidMargin   = errors.New("the winning point margin is invalid")
+	ErrUnneededSets    = errors.New("score contains unneeded extra sets")
+	ErrEqualSetWins    = errors.New("both opponents won an equal number of sets")
+)
+
+type scoreSettings struct {
+	WinningPoints, WinningSets, MaxPoints int
+	TwoPointMargin                        bool
+}
+
+func NewScoreSettings(
+	winningPoints, winningSets, maxPoints int,
+	twoPointMargin bool,
+) (scoreSettings, error) {
+	if !twoPointMargin {
+		maxPoints = winningPoints
+	}
+
+	scoreSettings := scoreSettings{
+		winningPoints, winningSets, maxPoints, twoPointMargin,
+	}
+
+	if winningPoints <= 0 {
+		return scoreSettings, ErrPointsZero
+	}
+	if winningSets <= 0 {
+		return scoreSettings, ErrSetsZero
+	}
+	if maxPoints < winningPoints {
+		return scoreSettings, ErrMaxPoints
+	}
+
+	return scoreSettings, nil
+}
+
+type score struct {
+	a, b []int
+}
+
+func (s *score) Points1() []int {
+	return s.a
+}
+
+func (s *score) Points2() []int {
+	return s.b
+}
+
+func (s *score) GetWinner() (int, error) {
+	setWins := 0
+	for i := range len(s.a) {
+		if s.a[i] > s.b[i] {
+			setWins += 1
+		}
+		if s.b[i] > s.a[i] {
+			setWins -= 1
+		}
+	}
+
+	if setWins > 0 {
+		return 0, nil
+	}
+	if setWins < 0 {
+		return 1, nil
+	}
+
+	return -1, ErrUndetermined
+}
+
+func (s *score) Invert() internal.Score {
+	score := &score{
+		a: s.b,
+		b: s.a,
+	}
+	return score
+}
+
+func NewScore(
+	a, b []int,
+	settings scoreSettings,
+) (*score, error) {
+	switch {
+	case len(a) == 0 || len(b) == 0:
+		return nil, ErrEmpty
+	case len(a) != len(b):
+		return nil, ErrUnequalSets
+	case len(a) < settings.WinningSets:
+		return nil, ErrTooFewSets
+	case len(a) >= 2*settings.WinningSets:
+		return nil, ErrTooManySets
+	}
+
+	winningMargin := 1
+	if settings.TwoPointMargin {
+		winningMargin = 2
+	}
+
+	setWinsA, setWinsB := 0, 0
+	for i := range len(a) {
+		w := max(a[i], b[i])
+		l := min(a[i], b[i])
+
+		switch {
+		case setWinsA == settings.WinningSets || setWinsB == settings.WinningSets:
+			return nil, ErrUnneededSets
+		case w == l:
+			return nil, ErrUndeterminedSet
+		case l < 0:
+			return nil, ErrNegativePoints
+		case w < settings.WinningPoints:
+			return nil, ErrTooFewPoints
+		case w > settings.MaxPoints:
+			return nil, ErrTooManyPoints
+		case w < settings.MaxPoints && w > settings.WinningPoints && w-l != winningMargin:
+			fallthrough
+		case w == settings.MaxPoints && w > settings.WinningPoints && w-l > winningMargin:
+			return nil, ErrInvalidMargin
+		}
+
+		if a[i] > b[i] {
+			setWinsA += 1
+		} else {
+			setWinsB += 1
+		}
+	}
+
+	if setWinsA == setWinsB {
+		return nil, ErrEqualSetWins
+	}
+
+	return &score{a, b}, nil
+}
---
+++ badminton/score_test.go
@@ -0,0 +1,227 @@
+package badminton
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestScoreSettings(t *testing.T) {
+	_, err := NewScoreSettings(0, 2, 30, true)
+	if err != ErrPointsZero {
+		t.Fatal("zero points did not error")
+	}
+
+	_, err = NewScoreSettings(21, 0, 30, true)
+	if err != ErrSetsZero {
+		t.Fatal("zero sets did not error")
+	}
+
+	_, err = NewScoreSettings(21, 2, 20, true)
+	if err != ErrMaxPoints {
+		t.Fatal("max points less than winning points did not error")
+	}
+
+	_, err = NewScoreSettings(21, 2, 21, true)
+	if err != nil {
+		t.Fatal("max points equal to winning points did error")
+	}
+
+	settings, err := NewScoreSettings(21, 2, 0, false)
+	if err != nil || settings.MaxPoints != 21 {
+		t.Fatal("max points not overridden when two point winning margin is false")
+	}
+
+	_, err = NewScoreSettings(21, 2, 30, true)
+	if err != nil {
+		t.Fatal("standard badminton score setting did error")
+	}
+}
+
+func TestScoreInterface(t *testing.T) {
+	settings, _ := NewScoreSettings(21, 2, 30, true)
+
+	a := []int{21, 21}
+	b := []int{1, 2}
+	score, err := NewScore(a, b, settings)
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	if !reflect.DeepEqual(a, score.Points1()) || !reflect.DeepEqual(b, score.Points2()) {
+		t.Fatal("score interface did not reproduce the given points")
+	}
+
+	inverted := score.Invert()
+	if !reflect.DeepEqual(a, inverted.Points2()) || !reflect.DeepEqual(b, inverted.Points1()) {
+		t.Fatal("score interface did not invert the given points")
+	}
+}
+
+func TestScoreErrors(t *testing.T) {
+	settings, _ := NewScoreSettings(21, 2, 30, true)
+
+	a := []int{}
+	b := []int{}
+	_, err := NewScore(a, b, settings)
+	if err != ErrEmpty {
+		t.Fatal("empty score did not error")
+	}
+
+	a = []int{21, 22}
+	b = []int{18}
+	_, err = NewScore(a, b, settings)
+	if err != ErrUnequalSets {
+		t.Fatal("unequal sets did not error")
+	}
+
+	a = []int{21}
+	b = []int{18}
+	_, err = NewScore(a, b, settings)
+	if err != ErrTooFewSets {
+		t.Fatal("too few sets did not error")
+	}
+
+	a = []int{21, 21, 21, 21}
+	b = []int{18, 18, 19, 7}
+	_, err = NewScore(a, b, settings)
+	if err != ErrTooManySets {
+		t.Fatal("too many sets did not error")
+	}
+
+	a = []int{21, 7, 10}
+	b = []int{23, 21, 21}
+	_, err = NewScore(a, b, settings)
+	if err != ErrUnneededSets {
+		t.Fatal("unneeded extra sets did not error")
+	}
+
+	a = []int{21, 7, 21}
+	b = []int{23, 21, 0}
+	_, err = NewScore(a, b, settings)
+	if err != ErrUnneededSets {
+		t.Fatal("unneeded extra sets did not error")
+	}
+
+	a = []int{21, 21}
+	b = []int{21, 18}
+	_, err = NewScore(a, b, settings)
+	if err != ErrUndeterminedSet {
+		t.Fatal("undetermined set did not error")
+	}
+
+	a = []int{21, 21}
+	b = []int{-1, 18}
+	_, err = NewScore(a, b, settings)
+	if err != ErrNegativePoints {
+		t.Fatal("negative points did not error")
+	}
+
+	a = []int{20, 21}
+	b = []int{17, 18}
+	_, err = NewScore(a, b, settings)
+	if err != ErrTooFewPoints {
+		t.Fatal("too few points did not error")
+	}
+
+	a = []int{31, 21}
+	b = []int{29, 18}
+	_, err = NewScore(a, b, settings)
+	if err != ErrTooManyPoints {
+		t.Fatal("too many points did not error")
+	}
+
+	a = []int{29, 21}
+	b = []int{28, 18}
+	_, err = NewScore(a, b, settings)
+	if err != ErrInvalidMargin {
+		t.Fatal("invalid winning margin did not error")
+	}
+
+	a = []int{30, 21}
+	b = []int{27, 18}
+	_, err = NewScore(a, b, settings)
+	if err != ErrInvalidMargin {
+		t.Fatal("invalid winning margin did not error")
+	}
+
+	a = []int{21, 7}
+	b = []int{7, 21}
+	_, err = NewScore(a, b, settings)
+	if err != ErrEqualSetWins {
+		t.Fatal("equal set wins did not error")
+	}
+}
+
+func TestValidScores(t *testing.T) {
+	settings, _ := NewScoreSettings(21, 2, 30, true)
+
+	a := []int{21, 21}
+	b := []int{10, 19}
+	score, err := NewScore(a, b, settings)
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	winner, _ := score.GetWinner()
+	if winner != 0 {
+		t.Fatal("score returned the wrong winner")
+	}
+
+	a = []int{0, 9}
+	b = []int{21, 21}
+	score, err = NewScore(a, b, settings)
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	winner, _ = score.GetWinner()
+	if winner != 1 {
+		t.Fatal("score returned the wrong winner")
+	}
+
+	a = []int{0, 21, 21}
+	b = []int{21, 17, 23}
+	score, err = NewScore(a, b, settings)
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	winner, _ = score.GetWinner()
+	if winner != 1 {
+		t.Fatal("score returned the wrong winner")
+	}
+
+	a = []int{0, 21, 0}
+	b = []int{21, 0, 21}
+	score, err = NewScore(a, b, settings)
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	winner, _ = score.GetWinner()
+	if winner != 1 {
+		t.Fatal("score returned the wrong winner")
+	}
+
+	settings, _ = NewScoreSettings(21, 2, 21, false)
+
+	a = []int{21, 21}
+	b = []int{20, 8}
+	score, err = NewScore(a, b, settings)
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	winner, _ = score.GetWinner()
+	if winner != 0 {
+		t.Fatal("score returned the wrong winner")
+	}
+
+	settings, _ = NewScoreSettings(21, 3, 21, false)
+
+	a = []int{21, 21, 20, 21}
+	b = []int{20, 8, 21, 10}
+	score, err = NewScore(a, b, settings)
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	winner, _ = score.GetWinner()
+	if winner != 0 {
+		t.Fatal("score returned the wrong winner")
+	}
+
+}
