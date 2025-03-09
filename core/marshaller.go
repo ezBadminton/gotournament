d03ba1a7a3f89463a72714e624761f2f08fa611f
Simon Gerstmeier <simon@gerstmeier.dev>
2025-03-09 18:39:37 +0100

    Boiled the information that the marshaller serializes down to what clients would actually need.

--- core/marshaller.go
+++ core/marshaller.go
@@ -1,77 +1,68 @@
 package core
 
 import (
-	"encoding/json"
 	"maps"
 )
 
-func marshalRankingsAndSlots(entries Ranking, rankingGraph *RankingGraph) map[string]any {
-	rankings := make(map[int][][]int)
-	slots := make(map[int]string)
+type TournamentMarshaller struct {
+	getMatchId func(i int) string
+}
 
-	for ranking := range rankingGraph.BreadthSearchIter(entries) {
-		rankingSlots := ranking.Ranks()
-		for _, slot := range rankingSlots {
-			_, ok := slots[slot.Id]
-			if ok {
-				continue
-			}
-			slots[slot.Id] = marshalSlot(slot)
-		}
-
-		rankings[ranking.Id()] = marshalRanking(ranking)
-	}
-
+func (m *TournamentMarshaller) marshalEntriesAndFinal(entries, finalRanking Ranking) map[string]any {
 	result := map[string]any{
-		"rankings": rankings,
-		"slots":    slots,
+		"entries":      m.marshalRanking(entries),
+		"finalRanking": m.marshalRanking(finalRanking),
 	}
-
 	return result
 }
 
-func marshalSlot(slot *Slot) string {
+func (m *TournamentMarshaller) marshalSlot(slot *Slot) map[string]any {
+	var occupant string
 	if slot.IsBye() {
 		if slot.Bye.Drawn {
-			return "db"
+			occupant = "db"
 		} else {
-			return "b"
+			occupant = "b"
 		}
 	} else if p := slot.Player; p != nil {
-		return p.Id()
+		occupant = p.Id()
 	} else {
-		return ""
+		occupant = ""
+	}
+	return map[string]any{
+		"id":       slot.Id,
+		"occupant": occupant,
 	}
 }
 
-func marshalRanking(ranking Ranking) [][]int {
+func (m *TournamentMarshaller) marshalRanking(ranking Ranking) [][]map[string]any {
 	switch r := ranking.(type) {
 	case TieableRanking:
-		return marshalTieableRanking(r)
+		return m.marshalTieableRanking(r)
 	default:
 		ranks := r.Ranks()
-		slotIds := make([][]int, len(ranks))
+		slotIds := make([][]map[string]any, len(ranks))
 		for i, slot := range ranks {
-			slotIds[i] = []int{slot.Id}
+			slotIds[i] = []map[string]any{m.marshalSlot(slot)}
 		}
 		return slotIds
 	}
 }
 
-func marshalTieableRanking(ranking TieableRanking) [][]int {
+func (m *TournamentMarshaller) marshalTieableRanking(ranking TieableRanking) [][]map[string]any {
 	ranks := ranking.TiedRanks()
-	slotIds := make([][]int, len(ranks))
+	slotIds := make([][]map[string]any, len(ranks))
 	for i, rank := range ranks {
-		rankSlotIds := make([]int, len(rank))
+		rankSlotIds := make([]map[string]any, len(rank))
 		for i, slot := range rank {
-			rankSlotIds[i] = slot.Id
+			rankSlotIds[i] = m.marshalSlot(slot)
 		}
 		slotIds[i] = rankSlotIds
 	}
 	return slotIds
 }
 
-func marshalTies(ties [][]*Slot) [][]int {
+func (m *TournamentMarshaller) marshalTies(ties [][]*Slot) [][]int {
 	tieIds := make([][]int, len(ties))
 	for i, tie := range ties {
 		slotIds := make([]int, len(tie))
@@ -83,7 +74,7 @@ func marshalTies(ties [][]*Slot) [][]int {
 	return tieIds
 }
 
-func marshalMetrics(ranking *MatchMetricRanking) []*MatchMetrics {
+func (m *TournamentMarshaller) marshalMetrics(ranking *MatchMetricRanking) []*MatchMetrics {
 	slots := ranking.Ranks()
 	metrics := make([]*MatchMetrics, 0, len(slots))
 	for _, s := range slots {
@@ -95,49 +86,47 @@ func marshalMetrics(ranking *MatchMetricRanking) []*MatchMetrics {
 	return metrics
 }
 
-func marshalMatchList(matchList *matchList) map[string]any {
-	rounds := make([][]map[string]any, len(matchList.Rounds))
-	for i, round := range matchList.Rounds {
-		roundMatches := make([]map[string]any, len(round.Matches))
-		for i, match := range round.Matches {
-			roundMatches[i] = marshalMatch(match)
+func (m *TournamentMarshaller) marshalMatchList(matchList *matchList) map[string]any {
+	rounds := make([][]*Match, len(matchList.Rounds))
+	for i, r := range matchList.Rounds {
+		rounds[i] = r.Matches
+	}
+	return m.marshalRoundList(rounds)
+}
+
+func (m *TournamentMarshaller) marshalRoundList(rounds [][]*Match) map[string]any {
+	matches := make([][]string, len(rounds))
+	matchI := 0
+	for i, round := range rounds {
+		roundMatches := make([]string, len(round))
+		for i := range round {
+			roundMatches[i] = m.getMatchId(matchI)
+			matchI += 1
 		}
-		rounds[i] = roundMatches
+		matches[i] = roundMatches
 	}
 
 	result := map[string]any{
-		"rounds": rounds,
+		"rounds": matches,
 	}
 
 	return result
 }
 
-func marshalMatch(match *Match) map[string]any {
-	score := make([][]int, 0)
-	if match.Score != nil {
-		score = append(
-			score,
-			match.Score.Points1(),
-			match.Score.Points2(),
-		)
-	}
-	var locationId string
-	if match.Location != nil {
-		locationId = match.Location.Id()
-	}
+func (m *TournamentMarshaller) marshalMatch(match *Match) map[string]any {
 	result := map[string]any{
-		"id":    match.id,
-		"slot1": match.Slot1.Id,
-		"slot2": match.Slot2.Id,
-		"score": score,
-		"start": match.StartTime.UnixMilli(),
-		"end":   match.EndTime.UnixMilli(),
-		"loc":   locationId,
+		"slot1":    m.marshalSlot(match.Slot1),
+		"slot2":    m.marshalSlot(match.Slot2),
+		"walkover": match.IsWalkover(),
+	}
+	winner, err := match.GetWinner()
+	if err == nil {
+		result["winner"] = winner.Player.Id()
 	}
 	return result
 }
 
-func marshalEditableMatches(editingPolicy EditingPolicy) map[string]any {
+func (m *TournamentMarshaller) marshalEditableMatches(editingPolicy EditingPolicy) map[string]any {
 	editable := editingPolicy.EditableMatches()
 	editableIds := make([]int, len(editable))
 	for i, match := range editable {
@@ -149,138 +138,156 @@ func marshalEditableMatches(editingPolicy EditingPolicy) map[string]any {
 	return result
 }
 
-func marshalSingleElimination(tournament *SingleElimination) map[string]any {
-	ranksAndSlots := marshalRankingsAndSlots(tournament.Entries, tournament.RankingGraph)
-	matchList := marshalMatchList(tournament.matchList)
-	editable := marshalEditableMatches(tournament)
+func (m *TournamentMarshaller) marshalSingleElimination(tournament *SingleElimination) map[string]any {
+	ranks := m.marshalEntriesAndFinal(tournament.Entries, tournament.FinalRanking)
+	matchList := m.marshalMatchList(tournament.matchList)
+	editable := m.marshalEditableMatches(tournament)
 	result := map[string]any{
 		"type": "SingleElimination",
 	}
 
-	maps.Copy(result, ranksAndSlots)
 	maps.Copy(result, matchList)
 	maps.Copy(result, editable)
+	maps.Copy(result, ranks)
 
 	return result
 }
 
-func marshalRoundRobin(tournament *RoundRobin) map[string]any {
-	ranksAndSlots := marshalRankingsAndSlots(tournament.Entries, tournament.RankingGraph)
-	matchList := marshalMatchList(tournament.matchList)
-	editable := marshalEditableMatches(tournament)
+func (m *TournamentMarshaller) marshalRoundRobin(tournament *RoundRobin) map[string]any {
+	ranks := m.marshalEntriesAndFinal(tournament.Entries, tournament.FinalRanking)
+	matchList := m.marshalMatchList(tournament.matchList)
+	editable := m.marshalEditableMatches(tournament)
+	numUntied := tournament.FinalRanking.RequiredUntiedRanks
+	ties := m.marshalTies(tournament.FinalRanking.BlockingTies(numUntied))
+	unbrokenTies := m.marshalTies(tournament.FinalRanking.BlockingUnbrokenTies(numUntied))
 	result := map[string]any{
-		"type":    "RoundRobin",
-		"metrics": marshalMetrics(tournament.FinalRanking),
+		"type":         "RoundRobin",
+		"metrics":      m.marshalMetrics(tournament.FinalRanking),
+		"ties":         ties,
+		"unbrokenTies": unbrokenTies,
 	}
 
-	maps.Copy(result, ranksAndSlots)
 	maps.Copy(result, matchList)
 	maps.Copy(result, editable)
+	maps.Copy(result, ranks)
 
 	return result
 }
 
-func marshalSingleEliminationWithConsolation(tournament *SingleEliminationWithConsolation) map[string]any {
-	ranksAndSlots := marshalRankingsAndSlots(tournament.Entries, tournament.RankingGraph)
-	mainBracket := marshalConsolationBracket(tournament.MainBracket)
-	editable := marshalEditableMatches(tournament)
+func (m *TournamentMarshaller) marshalSingleEliminationWithConsolation(tournament *SingleEliminationWithConsolation) map[string]any {
+	ranks := m.marshalEntriesAndFinal(tournament.Entries, tournament.FinalRanking)
+	mainBracket := m.marshalConsolationBracket(tournament.MainBracket)
+	editable := m.marshalEditableMatches(tournament)
 	result := map[string]any{
 		"type":        "SingleEliminationWithConsolation",
 		"mainBracket": mainBracket,
 	}
 
-	maps.Copy(result, ranksAndSlots)
 	maps.Copy(result, editable)
+	maps.Copy(result, ranks)
 
 	return result
 }
 
-func marshalConsolationBracket(bracket *ConsolationBracket) map[string]any {
-	matchList := marshalMatchList(bracket.matchList)
+func (m *TournamentMarshaller) marshalConsolationBracket(bracket *ConsolationBracket) map[string]any {
+	matchList := m.marshalMatchList(bracket.matchList)
 	nested := make([]map[string]any, len(bracket.Consolations))
 	for i, bracket := range bracket.Consolations {
-		nested[i] = marshalConsolationBracket(bracket)
+		nested[i] = m.marshalConsolationBracket(bracket)
 	}
 	result := map[string]any{
-		"matches":      matchList,
 		"consolations": nested,
 	}
+	maps.Copy(result, matchList)
+
 	return result
 }
 
-func marshalDoubleElimination(tournament *DoubleElimination) map[string]any {
-	ranksAndSlots := marshalRankingsAndSlots(tournament.Entries, tournament.RankingGraph)
-	matchList := marshalMatchList(tournament.matchList)
-	editable := marshalEditableMatches(tournament)
+func (m *TournamentMarshaller) marshalDoubleElimination(tournament *DoubleElimination) map[string]any {
+	ranks := m.marshalEntriesAndFinal(tournament.Entries, tournament.FinalRanking)
+	winnerMatchList := m.marshalMatchList(tournament.WinnerBracket.matchList)
+	loserMatchList := m.marshalRoundList(tournament.loserRounds)
+	editable := m.marshalEditableMatches(tournament)
 	result := map[string]any{
-		"type": "DoubleElimination",
+		"type":         "DoubleElimination",
+		"winnerRounds": winnerMatchList["rounds"],
+		"loserRounds":  loserMatchList["rounds"],
+		"final":        m.getMatchId(len(tournament.matchList.Matches) - 1),
 	}
 
-	maps.Copy(result, ranksAndSlots)
-	maps.Copy(result, matchList)
 	maps.Copy(result, editable)
+	maps.Copy(result, ranks)
 
 	return result
 }
 
-func marshalGroupPhase(tournament *GroupPhase) map[string]any {
-	groupMatchLists := make([]any, 0)
-	groupMetrics := make([][]*MatchMetrics, len(tournament.Groups))
-	groupTies := make([][][]int, len(tournament.Groups))
-	unbrokenGroupTies := make([][][]int, len(tournament.Groups))
+func (m *TournamentMarshaller) marshalGroupPhase(tournament *GroupPhase) map[string]any {
+	groups := make([]any, 0)
 	for _, g := range tournament.Groups {
-		matchList := marshalMatchList(g.matchList)
-		groupMatchLists = append(groupMatchLists, matchList["rounds"])
-
-		metrics := marshalMetrics(g.FinalRanking)
-		groupMetrics = append(groupMetrics, metrics)
-
+		matchList := m.marshalMatchList(g.matchList)
+		ranks := m.marshalEntriesAndFinal(g.Entries, g.FinalRanking)
 		numUntied := g.FinalRanking.RequiredUntiedRanks
-		ties := g.FinalRanking.BlockingTies(numUntied)
-		unbrokenTies := g.FinalRanking.BlockingUnbrokenTies(numUntied)
-		groupTies = append(groupTies, marshalTies(ties))
-		unbrokenGroupTies = append(unbrokenGroupTies, marshalTies(unbrokenTies))
+		ties := m.marshalTies(g.FinalRanking.BlockingTies(numUntied))
+		unbrokenTies := m.marshalTies(g.FinalRanking.BlockingUnbrokenTies(numUntied))
+		groupResult := map[string]any{
+			"type":         "GroupRoundRobin",
+			"metrics":      m.marshalMetrics(g.FinalRanking),
+			"ties":         ties,
+			"unbrokenTies": unbrokenTies,
+		}
+		maps.Copy(groupResult, matchList)
+		maps.Copy(groupResult, ranks)
+
+		groups = append(groups, groupResult)
 	}
 
 	ties := tournament.FinalRanking.CrossGroupTies()
-	crossGroupTies := marshalTies(ties)
+	unbrokenTies := tournament.FinalRanking.BlockingUnbrokenTies(tournament.FinalRanking.RequiredUntiedRanks)
+	crossGroupTies := m.marshalTies(ties)
+	unbrokenCrossGroupTies := m.marshalTies(unbrokenTies)
+	crossTiedRank, _ := tournament.FinalRanking.contestedRank()
 
 	result := map[string]any{
-		"type":              "GroupPhase",
-		"groupRounds":       groupMatchLists,
-		"groupMetrics":      groupMetrics,
-		"groupTies":         groupTies,
-		"unbrokenGroupTies": unbrokenGroupTies,
-		"crossGroupTies":    crossGroupTies,
+		"type":                   "GroupPhase",
+		"groups":                 groups,
+		"crossGroupTies":         crossGroupTies,
+		"unbrokenCrossGroupTies": unbrokenCrossGroupTies,
+		"crossTiedRank":          crossTiedRank,
 	}
 
 	return result
 }
 
-func marshalGroupKnockout(tournament *GroupKnockout) map[string]any {
-	ranksAndSlots := marshalRankingsAndSlots(tournament.Entries, tournament.RankingGraph)
-	groupPhase := marshalGroupPhase(tournament.GroupPhase)
-	editable := marshalEditableMatches(tournament)
+func (m *TournamentMarshaller) marshalGroupKnockout(tournament *GroupKnockout) map[string]any {
+	ranks := m.marshalEntriesAndFinal(tournament.Entries, tournament.FinalRanking)
+	groupPhase := m.marshalGroupPhase(tournament.GroupPhase)
+	editable := m.marshalEditableMatches(tournament)
 	var koPhase map[string]any
 	switch ko := tournament.KnockOutTournament.(type) {
 	case *SingleElimination:
-		matchList := marshalMatchList(ko.matchList)
+		matchList := m.marshalMatchList(ko.matchList)
+		ranks := m.marshalEntriesAndFinal(ko.Entries, ko.FinalRanking)
 		koPhase = map[string]any{
 			"type": "SingleElimination",
 		}
 		maps.Copy(koPhase, matchList)
+		maps.Copy(koPhase, ranks)
 	case *SingleEliminationWithConsolation:
-		mainBracket := marshalConsolationBracket(ko.MainBracket)
+		mainBracket := m.marshalConsolationBracket(ko.MainBracket)
+		ranks := m.marshalEntriesAndFinal(ko.Entries, ko.FinalRanking)
 		koPhase = map[string]any{
 			"type":        "SingleEliminationWithConsolation",
 			"mainBracket": mainBracket,
 		}
+		maps.Copy(koPhase, ranks)
 	case *DoubleElimination:
-		matchList := marshalMatchList(ko.matchList)
+		matchList := m.marshalMatchList(ko.matchList)
+		ranks := m.marshalEntriesAndFinal(ko.Entries, ko.FinalRanking)
 		koPhase = map[string]any{
 			"type": "DoubleElimination",
 		}
 		maps.Copy(koPhase, matchList)
+		maps.Copy(koPhase, ranks)
 	default:
 		panic("group knockout marshaller: unknown ko phase tournament type")
 	}
@@ -289,35 +296,41 @@ func marshalGroupKnockout(tournament *GroupKnockout) map[string]any {
 		"type":       "GroupKnockout",
 		"groupPhase": groupPhase,
 		"koPhase":    koPhase,
+		"koStarted":  tournament.KnockOut.matchList.MatchesStarted(),
 	}
 
-	maps.Copy(result, ranksAndSlots)
 	maps.Copy(result, editable)
+	maps.Copy(result, ranks)
 
 	return result
 }
 
-func (t *SingleElimination) MarshalJSON() ([]byte, error) {
-	anymap := marshalSingleElimination(t)
-	return json.Marshal(anymap)
+func (m *Match) ToMap() map[string]any {
+	marshaller := TournamentMarshaller{}
+	return marshaller.marshalMatch(m)
 }
 
-func (t *SingleEliminationWithConsolation) MarshalJSON() ([]byte, error) {
-	anymap := marshalSingleEliminationWithConsolation(t)
-	return json.Marshal(anymap)
+func (t *SingleElimination) ToMap(getMatchId func(int) string) map[string]any {
+	marshaller := TournamentMarshaller{getMatchId}
+	return marshaller.marshalSingleElimination(t)
 }
 
-func (t *RoundRobin) MarshalJSON() ([]byte, error) {
-	anymap := marshalRoundRobin(t)
-	return json.Marshal(anymap)
+func (t *SingleEliminationWithConsolation) ToMap(getMatchId func(int) string) map[string]any {
+	marshaller := TournamentMarshaller{getMatchId}
+	return marshaller.marshalSingleEliminationWithConsolation(t)
 }
 
-func (t *GroupKnockout) MarshalJSON() ([]byte, error) {
-	anymap := marshalGroupKnockout(t)
-	return json.Marshal(anymap)
+func (t *RoundRobin) ToMap(getMatchId func(int) string) map[string]any {
+	marshaller := TournamentMarshaller{getMatchId}
+	return marshaller.marshalRoundRobin(t)
 }
 
-func (t *DoubleElimination) MarshalJSON() ([]byte, error) {
-	anymap := marshalDoubleElimination(t)
-	return json.Marshal(anymap)
+func (t *GroupKnockout) ToMap(getMatchId func(int) string) map[string]any {
+	marshaller := TournamentMarshaller{getMatchId}
+	return marshaller.marshalGroupKnockout(t)
+}
+
+func (t *DoubleElimination) ToMap(getMatchId func(int) string) map[string]any {
+	marshaller := TournamentMarshaller{getMatchId}
+	return marshaller.marshalDoubleElimination(t)
 }
